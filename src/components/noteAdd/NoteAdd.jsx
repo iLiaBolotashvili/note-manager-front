fc40962d2cd7a39c5c94526cf86097f7b6457059
iLiaBolotashvili <iliabolotashvili1@gmail.com>
2023-06-27 11:45:01 +0400

    Add modal form for posting notes.

--- src/components/noteAdd/NoteAdd.jsx
+++ src/components/noteAdd/NoteAdd.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import AuthService from "../../services/auth.js";
 import axios from 'axios';
+import { Button, Modal, Label, TextInput, Textarea } from 'flowbite-react';
 
 const NoteAdd = () => {
 
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
+  const [openModal, setOpenModal] = useState(undefined);
+  const props = { openModal, setOpenModal };
+  
   const addTitleHandler = (event) => {
     setTitle(event.target.value);
   };
@@ -38,9 +42,27 @@ const NoteAdd = () => {
 
   return (
     <div>
-      <input type="text" value={title} onChange={addTitleHandler} placeholder="Title" />
-      <textarea value={content} onChange={addContentHandler} placeholder="Content" />
-      <button onClick={addNoteHandler}>Post Note</button>
+      <Button onClick={() => props.setOpenModal('dismissible')}>Add Note</Button>
+      <Modal dismissible show={props.openModal === 'dismissible'} onClose={() => props.setOpenModal(undefined)}>
+        <Modal.Header>Add a note</Modal.Header>
+        <Modal.Body>
+          <div className="py-2">
+            <Label
+              value="Title:"
+            />
+           <TextInput className='text-black' type="text" defaultValue={title} onBlur={addTitleHandler} placeholder="Title" required/>
+          </div>
+          <div className="py-2">
+            <Label
+              value="Content:"
+            />
+            <Textarea rows={12} className='text-black' type="text" defaultValue={content} onBlur={addContentHandler} placeholder="Content" required/>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={addNoteHandler}>Post Note</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
